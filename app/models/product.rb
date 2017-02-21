ea0d9f760e0a9b7b9f7e74a11657fac145392157
Mauricio Medina <mauricio.medina.munoz@gmail.com>
2017-02-21 00:05:36 -0300

    adding Premium

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :category
   validates :cost, presence: true
+  after_initialize :default_value 
+
+
+def default_value
+	self.premium =false
+
+end
+
 end
