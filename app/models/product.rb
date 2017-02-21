4d475c3feefd0d38262132ba7774cf704be786c8
Mauricio Medina <mauricio.medina.munoz@gmail.com>
2017-02-21 13:00:00 -0300

    instruction by seeds

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,14 @@
 class Product < ApplicationRecord
   belongs_to :category
   validates :cost, presence: true
-  after_initialize :default_value 
+  before_save :default_value
+  before_create :default_value 
 
 
 def default_value
+	unless self.premium.present? or self.premium==true
 	self.premium =false
+	end
 
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+Product.destroy_all
+
+Category.create(name: "bebidas")
+Category.create(name: "helados")
+Category.create(name: "conservas")
+
+Product.create(name: "fanta", cost:350, category_id: Category.find_by(name:"bebidas").id)
+
+new_product = Category.find_by(name:"bebidas")
+new_product.products.build(name:"pap_build", cost: 389).save
+
+Product.create(name: "chocolito", cost:700, category_id: Category.find_by(name:"helados").id)
+Product.create(name: "danky", cost:900, category_id: Category.find_by(name:"helados").id)
+
+Product.create(name: "duraznos", cost:1300, category_id: Category.find_by(name:"conservas").id)
+Product.create(name: "arvejas", cost:400, category_id: Category.find_by(name:"conservas").id)
+
+Product.last.destroy
+
+ult_registro = Product.last
+ult_registro.premium = true
+ult_registro.save
+
+categorias = Category.all
+categorias.each do |categoria|
+	categoria.name = categoria.name.upcase
+	categoria.save
+	end
+
