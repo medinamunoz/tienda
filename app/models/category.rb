3b7901fef40b414da78db49bd512ced26c8934c9
Mauricio Medina <mauricio.medina.munoz@gmail.com>
2017-02-21 17:56:43 -0300

    commintg discounts

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,13 @@
 class Category < ApplicationRecord
 	has_many :products, dependent: :destroy
+	validate :discount_invalid 
+
+
+	def discount_invalid
+		if discount < 0 or discount >100
+			errors.add(:discount, 'descuento debe ser entre 0 a 100')
+		end
+
+	end 
 end
+
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,21 +1,38 @@
 class Product < ApplicationRecord
 
-scope :premium_search, -> {where(premium: true)}
+	scope :premium_search, -> {where(premium: true)}
 
-scope :last_n, -> (n){order('id DESC').limit(n)}
+	scope :last_n, -> (n){order('id DESC').limit(n)}
+
+	after_destroy :category_delete
+
+	  belongs_to :category
+	  validates :cost, presence: true
+	  before_save :default_value
+	  before_create :default_value 
 
 
-  belongs_to :category
-  validates :cost, presence: true
-  before_save :default_value
-  before_create :default_value 
+	def default_value
+		unless self.premium.present? or self.premium==true
+		self.premium =false
+		end
 
-
-def default_value
-	unless self.premium.present? or self.premium==true
-	self.premium =false
 	end
 
-end
+	def category_delete
+		var = Category.all
+		var.each do |x|
+			if x.products.empty?
+				x.destroy
+			end
+		end
+	end
+
+
+	def precio_final
+		@aplica_descuento = self.cost - ((self.cost * self.category.discount).to_f / 100.0 )
+	end
+
+
 
 end
